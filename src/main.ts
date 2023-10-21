68379f4527596a0716abccc5ed39bdea87711de6
MrMugame <mr.mugame@gmail.com>
2023-10-21 23:20:28 +0200

    [New] New white Canvas

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,13 @@
+import { Lexer } from "./lib/lexer"
+import { TokenType } from "./lib/tokens";
 
-// fetch("code_pyramide.txt").then((res) => {
-//   return res.text()
-// }).then((res) => {
-//   let lexer = new Lexer(res);
-//   let tokens = lexer.tokenize();
+window.onload = () => {
+	let l = new Lexer("Anweisung { Test() \"Hello World\" *Anweisung {Comment");
 
-import { Handler } from "./lib/frontend/handler";
-import "./style/style.css"
-
-//   if (tokens instanceof LanguageError) return tokens
-
-//   console.log(tokens);
-
-//   let parser = new Parser(tokens);
-//   let ast = parser.parse();
-//   console.log(ast);
-
-//   if (ast instanceof LanguageError) return ast
-
-//   const ctx = document.querySelector("#three");
-//   if (!ctx) return
-//   let world = new World(ctx);
-//   let interpreter = new Interpreter(world, ast);
-//   let gen = interpreter.run()
-//   const run = () => {
-//       world.animate();
-//       gen.next();
-//       window.requestAnimationFrame(run.bind(this));
-//   }
-//   run()
-
-
-//   let button = document.querySelector("#btn")
-//   button?.addEventListener("click", () => {
-//     gen.next();
-//   })
-// })
-
-
-const handler = new Handler({
-    workspace: "#three",
-    editor: "#editor",
-    output: '#out',
-    playbutton: '#play',
-    pausebutton: '#pause',
-    fastbutton: '#fast',
-    stepbutton: '#step',
-    resetbutton: '#reset'
-});
+	let i = 0;
+	while (l.currToken.type != TokenType.EOF && i < 100) {
+		l.next_token();
+		console.log(l.currToken);
+		i++;
+	}
+}
