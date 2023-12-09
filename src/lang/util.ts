d054b29ef0e459592f3a13a5a6518518a5fa5951
MrMugame <mr.mugame@gmail.com>
2023-12-09 19:28:36 +0100

    [New] Added flatten for Results

--- src/lang/util.ts
+++ src/lang/util.ts
@@ -8,17 +8,23 @@ class Result<T, E extends Error = Error> {
     public static Ok = <T>(val: T): Result<T, any> => new Result(true, val, null);
     public static Err = <E extends Error>(err: E): Result<any, E> => new Result(false, null, err);
 
-    public unwrap = (): T => {
+    public unwrap(): T {
         assert(this.value != null, "Couldn't unwrap result because it was null");
         return this.value!;
     }
-    public unwrapErr = (): E => {
+    public unwrapErr(): E {
         assert(this.error != null, "Couldn't unwrap error from result because it was null");
         return this.error!;
     }
 
     public isOk = (): boolean => this.ok;
 }
+
+const flatten = <T>(result: Result<Result<T>>): Result<T> => {
+    if (result.isOk()) return result.unwrap();
+    else return Result.Err(result.unwrapErr());
+}
+
 
 const copy = <T = Object>(value: T): T => <T>{...value};
 
@@ -33,4 +39,4 @@ const assert = (condition: boolean, msg: string): void => {
     throw new Error(msg);
 }
 
-export { Result, copy, is, assert }
+export { Result, copy, is, assert, flatten }
