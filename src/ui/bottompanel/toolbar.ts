7dccb6380ff5bdb2182371c82a9876a6160a0fed
MrMugame <mr.mugame@gmail.com>
2024-02-15 22:46:58 +0100

    [Fix] Fixed right turning button

--- src/ui/bottompanel/toolbar.ts
+++ src/ui/bottompanel/toolbar.ts
@@ -25,6 +25,10 @@ document.querySelector("#btn-left")!.addEventListener("click", () => {
     controller.getWorld().rotateLeft();
 });
 
+document.querySelector("#btn-right")!.addEventListener("click", () => {
+    controller.getWorld().rotateRight();
+});
+
 document.querySelector("#btn-forward")!.addEventListener("click", () => {
     controller.getWorld().step();
 });
