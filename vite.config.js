958f307d5552f3e083ca751e068469080da03526
MrMugame <mr.mugame@gmail.com>
2024-02-16 23:57:29 +0100

    [Fix] Fix base

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { lezer } from "@lezer/generator/rollup"
 import { plugin, Mode } from "vite-plugin-markdown"
 
 export default {
+    base: "/webkarol",
     plugins: [lezer(), plugin({mode: Mode.HTML})]
 }
